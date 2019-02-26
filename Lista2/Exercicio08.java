9980481d9af0017cbc0482a09006b439dc9547e1
Eric Vier <goustkor@outlook.com>
2019-02-26 15:42:35 -0300

    Add files via upload

--- Lista2/Exercicio08.java
+++ Lista2/Exercicio08.java
@@ -21,20 +21,22 @@ Scanner in = new Scanner(System.in);
 		System.out.println("Digite o valor 3: ");
 		v3 = Double.parseDouble(in.nextLine());
 		
-		if(v1 > v2) {
-			if(v2 > v3) {
-				System.out.println("Valor 1: " + v1 + "\n" + "Valor 2: " + v2 + "\n" + "Valor 3: " + v3);
-			}else {
-				System.out.println("Valor 1: " + v1 + "\n" + "Valor 3: " + v3 + "\n" + "Valor 2: " + v2);
+		if(v1 > v2) {  // v1 .. v2
+			if(v1 > v3) {  // v1 .. v2 v1 ... 3
+				if(v2 > v3) { //v1 .. v2 ... v3
+					System.out.println("Valor 1: " + v1 + "\n" + "Valor 2: " + v2 + "\n" + "Valor 3: " + v3);
+				}else { //v1 .. v3 .. v2
+					System.out.println("Valor 1: " + v1 + "\n" + "Valor 3: " + v3 + "\n" + "Valor 2: " + v2);
+				}
+			}else { // v1 .. v2 v3 ... v1
+				System.out.println("Valor 3: " + v3 + "\n" + "Valor 1: " + v1 + "\n" + "Valor 2: " + v2);
 			}
-		}else if(v2 > v3) {
-			if(v1 > v3) {
+		}else { // v2 .. v1
+			if(v1 > v3) { //v2 .. v1 v1 .. v3
 				System.out.println("Valor 2: " + v2 + "\n" + "Valor 1: " + v1 + "\n" + "Valor 3: " + v3);
-			}else {
+			}else if(v2 > v3){ // v2 .. v1 v3 .. v1 v2 .. v3
 				System.out.println("Valor 2: " + v2 + "\n" + "Valor 3: " + v3 + "\n" + "Valor 1: " + v1);
-			}
-		}else if(v2 < v3) {
-			if(v3 > v2) {
+			}else { // v2 .. v1 v3 .. v1 v3 .. v2
 				System.out.println("Valor 3: " + v3 + "\n" + "Valor 2: " + v2 + "\n" + "Valor 1: " + v1);
 			}
 		}
